b9f599c03d90dbbeea7ab529f4e1f4656ae0781a
christopherseaman <86775+christopherseaman@users.noreply.github.com>
2021-03-01 20:25:42 -0800

    minor changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,6 @@
 # Update and deps
 apt-get update
 
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-
 # Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
@@ -24,5 +16,5 @@ apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 # Compose code-server
-docker-compose -f code-server/docker-compose.yaml up --build --detach
+docker-compose up --build --detach
 #DEBUG sudo -H SUDO_PASSWORD=$SUDO_PASSWORD CODE_PASSWORD=$CODE_PASSWORD docker-compose -f code-server/docker-compose.yaml up --build --detach
